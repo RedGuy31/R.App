d1e5b0d0ed86224a7fe345c82cc815f4d6eb7df6
Red guy <i.maisuradze31.19@gmail.com>
2021-07-06 15:03:33 -0700

    app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Red1 from './img/1.gif'
+import Red2 from './img/2.png'
+import Red3 from './img/3.png'
+import SingleComment from './SingleComment';
+import UserCard from './UserCard';
+
+const App = () => {
+    return(
+        <div className='ui comments'>
+            <UserCard>
+                <div>
+                    Hello i am Red Butt guy Again!!!
+                </div>
+            </UserCard>
+            <UserCard>
+            <SingleComment name='Red Guy'
+             date='Todat at 5:00pm' 
+             text='I am Red Ass Devil'
+             picture= {Red1}/>
+             </UserCard>
+             <br></br>
+             <UserCard>
+            <SingleComment name='Baboon' 
+            date=' Today at 5:20pm'
+            text='I R Baboon'
+            picture={Red3} />
+            </UserCard>
+            <br></br>
+            <UserCard>
+            <SingleComment name='Weasol'
+             date='Today at 5:25pm'
+             text='I am Weasol'
+             picture={Red2} />
+        </UserCard>
+            </div>
+       
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
